012b6e6b0d52e118c9a2199aa2858e5a99c7a481
unknown <nata13121986.knata1312@gmail.com>
2020-09-22 20:39:58 -0500

    JS

---
+++ Ex_19/index.js
@@ -0,0 +1,17 @@
+'use strict';
+const birthMonth = "December";
+const firstName = "Natali";
+const LastName = "Fox";
+let city;
+city = "Winnipeg";
+let age;
+age = 25;
+let nextCity;
+nextCity = city;
+
+console.log('City: ', city);
+console.log('Age: ', age);
+console.log('Next City: ', nextCity);
+alert (`Customer ${firstName}, ${LastName}`);
+console.log(`'Customer ${firstName}, ${LastName}'`);
+alert ("Hello,world!");
