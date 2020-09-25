a8430adf60e59695fe966dbf16dc8dbac237329c
unknown <nata13121986.knata1312@gmail.com>
2020-09-25 18:51:39 -0500

    Ex_22

---
+++ Ex_22/index.js
@@ -0,0 +1,13 @@
+"use striect"
+alert ('Hello, user!');
+let firstNum = Number(prompt ('Please, enter first number', 100));
+let secondNum = Number(prompt ('Please, enter second number', 100));
+
+if (firstNum > secondNum){
+    alert('The first number is greater than second number');
+}
+else if (firstNum < secondNum) {
+      alert ('The first number is less then secong number!');
+}
+else {alert('The numbers are the same!')}
+
