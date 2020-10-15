1d77478fd46c8dbf28766f0721ad8ce38c1b5f4c
nata13120403 <71297440+nata13120403@users.noreply.github.com>
2020-10-15 17:51:29 -0500

    Update index1.js

--- Ex_22/index1.js
+++ Ex_22/index1.js
@@ -3,10 +3,10 @@ let curHour = Number(prompt ('Please, enter fwtat hour is it (from 0-23)', 100))
 if (curHour < 0 || curHour > 23) {
     alert ("Your input incorrect");
 }
-else if (curHour < 8 || curHour == 18 ) {
+else if (curHour < 8 || curHour == 18 || curHour > 18) {
     alert ('It is not working hours');
 }
-else (curHour > 8 || curHour == 8 && curHour < 18) ;
+else 
 {
     alert ('We are open!!!');
 }
